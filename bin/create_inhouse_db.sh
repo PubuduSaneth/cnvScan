9e2c0d771ade5bcb3912727ae8094083c1a0aa2d
Hugues Fontenelle <hugues.fontenelle@medisin.uio.no>
2016-09-06 14:37:47 +0200

    Anonymize inhouse db
    Not a full anonymization (this would not be possible), but at least the real sample names are erased

--- bin/create_inhouse_db.sh
+++ bin/create_inhouse_db.sh
@@ -2,10 +2,16 @@
 
 set -euf -o pipefail
 
+mkdir -p DB
+rm -f DB/DB.txt
+
+i=0
 for f in $(find . -name "*.cnvScan.bed" -type f)
 do
-    awk 'BEGIN{OFS="\t"}{print $0, substr(FILENAME,3)}' $f >> DB/DB.txt
+    echo $f
+    awk 'BEGIN{OFS="\t"}{printf "%s\tsample%03i\n", $0, $i}' $f >> DB/DB.txt
+    i=$(($i+1))
 done
 sort -k1,1 -k2n,2 -k3n,3 DB/DB.txt > DB/DB.txt.sorted.bed
-bgzip DB/DB.txt.sorted.bed
+bgzip -f DB/DB.txt.sorted.bed
 tabix -p bed DB/DB.txt.sorted.bed.gz
