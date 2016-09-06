ece20441ff8ef5d3940db43ab62764f408ced434
Hugues Fontenelle <hugues.fontenelle@medisin.uio.no>
2016-09-06 15:51:42 +0200

    [feature] argparse: argument parser

--- bin/run_cnvScan.sh
+++ bin/run_cnvScan.sh
@@ -7,5 +7,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for f in $(find . -name "*.cnvScan.bed" -type f)
 do
     wc -l $f
-    python ${DIR}/../src/cnvScan_run.py $f ${f%.*}.out.tab ${DIR}/../data/DB/DB.txt.sorted.bed.gz
+    python ${DIR}/../src/cnvScan_run.py \
+        -i $f \
+        -o ${f%.*}.out.tab \
+        -db ${DIR}/../data/DB/DB.txt.sorted.bed.gz \
+        --resources ${DIR}/../resources/                    
 done
