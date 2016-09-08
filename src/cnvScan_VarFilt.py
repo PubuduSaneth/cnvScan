784387dbe4cfb7a9897827ec1d4ac334e730c9df
Hugues Fontenelle <hugues.fontenelle@medisin.uio.no>
2016-09-08 11:40:34 +0200

    [feature] Add argparse

--- src/cnvScan_VarFilt.py
+++ src/cnvScan_VarFilt.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-import glob
 import pysam
 import sys
+import argparse
 
 '''
 Run command
@@ -48,79 +48,105 @@ Field IDs
 
 '''
 
-cnvQual = int(sys.argv[1])
-filename = sys.argv[2]
+class cnv_filter(object):
 
-if len(sys.argv) > 3:
-	gene_list = pysam.TabixFile(sys.argv[3])
-
-print """Filter protocol
-            4 score > %d and
-            7 inDB_MinMaxMedian.split("|")[2] > %d and
-            24 DGV_Stringency2_count == "NA" OR 26 DGV_Stringency12_count == "NA" and
-            28 1000g_del == "NA"
-            29 1000g_ins == "NA" """ %(cnvQual, cnvQual)
-
-if len(sys.argv) > 3:
-	print "Input \t0:%r \n\t1:%r \n\t2:%r \n\t3:%r" %(sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3])
-else:
-	print "Input \t0:%r \n\t1:%r \n\t2:%r" %(sys.argv[0], sys.argv[1], sys.argv[2])
-
-out_fName = "cnvScan_Rare"+"_ScoreFilt-"+str(cnvQual)+".tab"
-out_file = open(out_fName, 'w')
+    def __init__(self, input_file, output_file, score, genelist):
+        self.input = input_file
+        self.output = output_file
+        self.score = score
+        self.genelist = genelist
+        self.process()
 
 
-out_file.write ("""Filtration protocol
-            Default score > %d and
-            CNVQ: inDB_MinMaxMedian.split("|")[2] > %d and
-            DGV CNVs: DGV_Stringency2_count == "NA" OR 26 DGV_Stringency12_count == "NA" and
-            1000 genome deletion: 1000g_del == "NA"
-            1000 genome insertion: 1000g_ins == "NA"
-""" %(cnvQual, cnvQual))
+    def process(self):
+		if self.genelist:
+			gene_list = pysam.TabixFile(self.genelist)
 
-if len(sys.argv) > 3:
-	out_file.write("\t".join(['chr', 'start', 'end', 'cnv_state', 'default_score', 'len', 'inDB_count', 'inDBScore_MinMaxMedian', 'gene_name', 'gene_type', 'gene_id', 'exon_count', 'UTR', 'transcript', 'phastConElement_count', 'phastConElement_minMax', 'haplo_insufIdx_count', 'haplo_insufIdx_score', 'Gene_intolarance_score', 'sanger_cnv', 'dgv_cnv', 'dgv_varType', 'dgv_varSubType', 'dgv_pubmedId', 'DGV_Stringency2_count', 'DGV_Stringency2_PopFreq', 'DGV_Stringency12_count', 'DGV_Stringency12_popFreq', '1000g_del', '1000g_ins', 'omim_morbidMap', 'ddd_mutConsequence', 'ddd_diseaseName', 'ddd_pubmedId', 'clinVar_disease', 'hgvs_varName', 'PIDD_GENE', 'Inheritnce', 'Phenotype']) + "\n")
-else:
-	out_file.write("\t".join(['chr', 'start', 'end', 'cnv_state', 'default_score', 'len', 'inDB_count', 'inDBScore_MinMaxMedian', 'gene_name', 'gene_type', 'gene_id', 'exon_count', 'UTR', 'transcript', 'phastConElement_count', 'phastConElement_minMax', 'haplo_insufIdx_count', 'haplo_insufIdx_score', 'Gene_intolarance_score', 'sanger_cnv', 'dgv_cnv', 'dgv_varType', 'dgv_varSubType', 'dgv_pubmedId', 'DGV_Stringency2_count', 'DGV_Stringency2_PopFreq', 'DGV_Stringency12_count', 'DGV_Stringency12_popFreq', '1000g_del', '1000g_ins', 'omim_morbidMap', 'ddd_mutConsequence', 'ddd_diseaseName', 'ddd_pubmedId', 'clinVar_disease', 'hgvs_varName']) + "\n")
+		print """Filter protocol
+		            4 score > %d and
+		            7 inDB_MinMaxMedian.split("|")[2] > %d and
+		            24 DGV_Stringency2_count == "NA" OR 26 DGV_Stringency12_count == "NA" and
+		            28 1000g_del == "NA"
+		            29 1000g_ins == "NA" """ %(self.score, self.score)
+
+		out_file = open(self.output, 'w')
+
+		out_file.write ("""Filtration protocol
+		            Default score > %d and
+		            CNVQ: inDB_MinMaxMedian.split("|")[2] > %d and
+		            DGV CNVs: DGV_Stringency2_count == "NA" OR 26 DGV_Stringency12_count == "NA" and
+		            1000 genome deletion: 1000g_del == "NA"
+		            1000 genome insertion: 1000g_ins == "NA"\n""" %(self.score, self.score))
+
+		if self.genelist:
+			out_file.write("\t".join(['chr', 'start', 'end', 'cnv_state', 'default_score', 'len', 'inDB_count', 'inDBScore_MinMaxMedian', 'gene_name', 'gene_type', 'gene_id', 'exon_count', 'UTR', 'transcript', 'phastConElement_count', 'phastConElement_minMax', 'haplo_insufIdx_count', 'haplo_insufIdx_score', 'Gene_intolarance_score', 'sanger_cnv', 'dgv_cnv', 'dgv_varType', 'dgv_varSubType', 'dgv_pubmedId', 'DGV_Stringency2_count', 'DGV_Stringency2_PopFreq', 'DGV_Stringency12_count', 'DGV_Stringency12_popFreq', '1000g_del', '1000g_ins', 'omim_morbidMap', 'ddd_mutConsequence', 'ddd_diseaseName', 'ddd_pubmedId', 'clinVar_disease', 'hgvs_varName', 'PIDD_GENE', 'Inheritnce', 'Phenotype']) + "\n")
+		else:
+			out_file.write("\t".join(['chr', 'start', 'end', 'cnv_state', 'default_score', 'len', 'inDB_count', 'inDBScore_MinMaxMedian', 'gene_name', 'gene_type', 'gene_id', 'exon_count', 'UTR', 'transcript', 'phastConElement_count', 'phastConElement_minMax', 'haplo_insufIdx_count', 'haplo_insufIdx_score', 'Gene_intolarance_score', 'sanger_cnv', 'dgv_cnv', 'dgv_varType', 'dgv_varSubType', 'dgv_pubmedId', 'DGV_Stringency2_count', 'DGV_Stringency2_PopFreq', 'DGV_Stringency12_count', 'DGV_Stringency12_popFreq', '1000g_del', '1000g_ins', 'omim_morbidMap', 'ddd_mutConsequence', 'ddd_diseaseName', 'ddd_pubmedId', 'clinVar_disease', 'hgvs_varName']) + "\n")
 
 
-def filt_line(f_line):
-	r_line = []
+		def filt_line(f_line):
+			r_line = []
 
-	if float(f_line[4]) > cnvQual and \
-		(f_line[7] == "NA" or ( not f_line[7] == "NA" and float(f_line[7].split("|")[2]) > cnvQual)) and \
-		(f_line[24] == "NA" or f_line[26] == "NA") and \
-		(f_line[28] == "NA" or f_line[29] == "NA"):
-		r_line = f_line
-	return r_line
+			if float(f_line[4]) > self.score and \
+				(f_line[7] == "NA" or ( not f_line[7] == "NA" and float(f_line[7].split("|")[2]) > self.score)) and \
+				(f_line[24] == "NA" or f_line[26] == "NA") and \
+				(f_line[28] == "NA" or f_line[29] == "NA"):
+				r_line = f_line
+			return r_line
 
-with open(filename, "r") as f1:
+		with open(self.input, "r") as f1:
 
-	next(f1)
-	for line in f1:
+			next(f1)
+			for line in f1:
 
-		g_name = []
-		inheritance = []
-		phenotype = []
-		line = line.replace("\n","")
-		line = line.replace("\r","")
-		line = line.split("\t")
-		if len(sys.argv) > 3:
-			try:
-				for row in gene_list.fetch(str(line[0]), int(line[1]), int(line[2])):
-					row = row.split("\t")
-					g_name.append(row[3])
-					inheritance.append(row[4])
-					phenotype.append(row[5])
-				if len(g_name) >0 :
-					line.extend([ "|".join(g_name), "|".join(inheritance), "|".join(phenotype)])
+				g_name = []
+				inheritance = []
+				phenotype = []
+				line = line.replace("\n","")
+				line = line.replace("\r","")
+				line = line.split("\t")
+				if self.genelist:
+					try:
+						for row in gene_list.fetch(str(line[0]), int(line[1]), int(line[2])):
+							row = row.split("\t")
+							g_name.append(row[3])
+							inheritance.append(row[4])
+							phenotype.append(row[5])
+						if len(g_name) >0 :
+							line.extend([ "|".join(g_name), "|".join(inheritance), "|".join(phenotype)])
+							p_line = filt_line(line)
+							if len(p_line) > 0:
+								out_file.write("\t".join(p_line)+"\n")
+					except ValueError:
+						pass
+				else:
 					p_line = filt_line(line)
 					if len(p_line) > 0:
 						out_file.write("\t".join(p_line)+"\n")
-			except ValueError:
-				pass
-		else:
-			p_line = filt_line(line)
-			if len(p_line) > 0:
-				out_file.write("\t".join(p_line)+"\n")
-out_file.close()
+		out_file.close()
+
+
+# ============================================================
+def main():
+        '''
+        cnvScan_VarFilt
+        '''
+        parser = argparse.ArgumentParser(
+            description='Filter annotated CNV prediction based on rules')
+        parser.add_argument('-i', '--input', type=str, required=True,
+                            help='Input CNV bed file')
+        parser.add_argument('-o', '--output', type=str, required=True,
+                            help='Output CNV bed file')
+        parser.add_argument('-s', '--score', type=float, required=False,
+							default=10.0,
+                            help='Quality score threshold')
+        parser.add_argument('-g', '--genelist', type=str, required=False,
+                            help='Gene list')
+        args = parser.parse_args()
+
+        filtered = cnv_filter(args.input, args.output, args.score, args.genelist)
+
+
+# ============================================================
+if __name__ == "__main__":
+    sys.exit(main())
